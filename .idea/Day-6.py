1ef3447ae28a995b918873136a0c09dfb6b2b9c3
VikasSinga <singamsettivikas666@gmail.com>
2025-04-11 11:16:28 +0530

    Day-4

---
+++ .idea/Day-6.py
@@ -0,0 +1,96 @@
+print("hello")
+num_char=len("hello")
+print((num_char ))
+
+# functions
+def My_function():
+    print("Hi dilli")
+    print("Naku")
+My_function()
+
+# indendation is very important
+# .... four dots of space
+
+# While Loop:
+
+
+# while somtimg_is_true:
+
+# MY CODE
+
+# def turn_right():
+# turn_left()
+# turn_left()
+# turn_left()
+# def Huddle_race():
+# #for robat in range(0,6):
+# #huddles = 6
+# #while huddles > 0:
+# #while at_goal() != True:
+# move()
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_left()
+
+
+# Aenglas Code
+
+
+# while not at_goat():
+#     huddle_race()
+#
+# Tall huddle My_code
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def Huddle_race():
+#     #for robat in range(0,6):
+#     #huddles = 6
+#     #while huddles > 0 :
+#     #while at_goal() != True:
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     # huddles -= 1
+# while not at_goal():
+#     if wall_on_right() and wall_in_front():
+#         turn_left()
+#     elif front_is_clear() and not right_is_clear():
+#         move()
+#     else:
+#         Huddle_race()
+
+
+# Aengelas Code
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def Huddle_race():
+#     #for robat in range(0,6):
+#     #huddles = 6
+#     #while huddles > 0 :
+#     #while at_goal() != True:
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+# # huddles -= 1
+# while not at_goal():
+#     if wall_in_front():
+#         Huddle_race()
+#     else:
+#         move()
